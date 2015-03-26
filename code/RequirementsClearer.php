13604365a5d2a60e391e3aeeac4c93f6fff1d9c2
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-03-26 13:27:11 +0700

    ENHANCEMENT: Addition of method to clear requirements from a config file

--- code/RequirementsClearer.php
+++ code/RequirementsClearer.php
@@ -1,7 +1,31 @@
-<?php 
+<?php
 
 class RequirementsClearer extends DataExtension {
+	/**
+	 * Clear all requirements.  Call this from a template using $ClearAllRequirements
+	 */
 	public function ClearAllRequirements() {
 		Requirements::clear();
+	}
+
+
+	/**
+	 * Clear a list of JS and CSS files from a configuration file
+	 * See README.md for details
+	 */
+	public function ClearCustomRequirements() {
+		$config = Config::inst();
+
+		// clear the JavaScript files
+		$jsfiles = $config->get('RequirementsClearer', 'JavaScript');
+		foreach ($jsfiles as $jsfile) {
+			Requirements::block($jsfile);
+		}
+
+		// clear the CSS files
+		$cssfiles = $config->get('RequirementsClearer', 'CSS');
+		foreach ($cssfiles as $cssfile) {
+			Requirements::block($cssfile);
+		}
 	}
 }
