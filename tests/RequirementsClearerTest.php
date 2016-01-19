d065ad44748cea4cc460f7d12067494d6cf83abb
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-19 23:36:16 +0700

    WIP: Initial tests shell

---
+++ tests/RequirementsClearerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class RequirementsClearerTest extends SapphireTest {
+	public function testClearAllRequirements() {
+		$this->markTestSkipped('TODO');
+	}
+
+	public function testClearCustomRequirements() {
+		$this->markTestSkipped('TODO');
+	}
+
+}
